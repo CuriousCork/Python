eea1ff1598e19f0048cf31165fa319499fb6925d
CuriousCork <184484640+CuriousCork@users.noreply.github.com>
2025-02-18 08:35:20 +0000

    added Matrix

---
+++ Matrix.py
@@ -0,0 +1,19 @@
+'''
+    Programador......: (C) 2025. Diogo Ramiro
+    Data.............: 17/02/25
+    Observações......: Matrizes
+'''
+
+import numpy as np
+matriz1D = np.array([1, 2, 3, 4])
+
+print(matriz1D)
+print(matriz1D.dtype)
+resultadoSoma = np.sum(matriz1D)
+print(resultadoSoma)
+
+matriz2D = np.array([[0, 0, 1], [1, 1, 0], [1, 1, 1]])
+print(matriz2D)
+print(matriz2D.dtype)
+
+ 
